9de5543b8c9226c9f9d74575758aef1142a13e49
vinndz <jeremykev186@gmail.com>
2022-09-14 17:56:34 +0700

    Initial commit

---
+++ app/src/main/java/com/example/view_binding_10852/Task.kt
@@ -0,0 +1,6 @@
+package com.example.view_binding_10852
+
+import android.telecom.Call
+
+data class Task (val chapter:String, val title:String, val details:String){
+}
---
+++ app/src/main/java/com/example/view_binding_10852/TaskList.kt
@@ -0,0 +1,14 @@
+package com.example.view_binding_10852
+
+object TaskList {
+    val taskList = listOf<Task>(
+        Task("Chapter 1", "Bangun Tidur",
+            "Kebiasaan sehari-hari untuk membuat hari makin Indah"),
+        Task("Chapter 2", "Nguli Genshin Impact", "Mencari Primo buat Gacha mama Yaemiko tercinta/Keging Wangi"),
+        Task("Chapter 3", "Ngopi", "Habis capek Nguli kan enak ngopi ya kan"),
+        Task("Chapter 4", "dimana mana Ei?", "Buat Adu Mekani Showcase Ayaka Pyro"),
+        Task("Chapter 5", "Upload ke Tiktok",
+        "Buat Pamer ke view Showcase Ayaka Pyro C6 + Bennet C6")
+    )
+
+}
